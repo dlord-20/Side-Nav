9e9dedcd2fe985b54735e60d9c27d0361f431bd7
dlord-20 <dthomaslord@gmail.com>
2022-08-07 14:46:20 +0300

    Started to implement color theme switch. Tested one element in css and js together -> Working so far

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+//Making the content align correct with navbar extending or minimizing
+
 const content = document.getElementsByClassName('main-container');
 const navCheckbox = document.querySelector("input[name=navcheckbox]");
 
-console.log(navCheckbox);
-
+// console.log(navCheckbox);
 
 navCheckbox.addEventListener('change', function() {
     if(this.checked) {
@@ -13,3 +14,17 @@ navCheckbox.addEventListener('change', function() {
     }
 });
 
+// Switching from dark to light mode
+
+const themeSelected = document.querySelector("input[name=navtheme]");
+console.log(themeSelected);
+
+//Get the needed element to change colors
+const innerToggle = document.getElementsByClassName('inner-toggle');
+console.log(innerToggle[0]);
+
+// Testing color change
+
+innerToggle[0].classList.add('light');
+
+// Seems to be working
