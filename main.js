a3ac46131708894077ce6ccbaaae9fee40480259
dlord-20 <dthomaslord@gmail.com>
2022-08-07 15:14:14 +0300

    Basic theme switch is now working but we need to have the logo change accordingly as well.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const navCheckbox = document.querySelector("input[name=navcheckbox]");
 navCheckbox.addEventListener('change', function() {
     if(this.checked) {
         content[0].classList.add('active')
-        console.log('Checked');
+        // console.log('Checked');
     } else {
         content[0].classList.remove('active')
     }
@@ -20,11 +20,46 @@ const themeSelected = document.querySelector("input[name=navtheme]");
 console.log(themeSelected);
 
 //Get the needed element to change colors
+const outerToggle = document.getElementsByClassName('outer-toggle');
 const innerToggle = document.getElementsByClassName('inner-toggle');
-console.log(innerToggle[0]);
+const navCheckboxElement = document.getElementsByClassName('nav-checkbox');
+const navExtend = document.getElementsByClassName('nav-extend');
+const navContainer = document.getElementsByClassName('nav-container');
+const darkModeToggle = document.getElementsByClassName('dark-mode-toggle');
+const navTheme = document.getElementsByClassName('nav-theme');
+const navBox = document.getElementsByClassName('nav-box');
 
-// Testing color change
+// nav-container
 
-innerToggle[0].classList.add('light');
 
-// Seems to be working
+themeSelected.addEventListener('change', function() {
+    if(this.checked) {
+        outerToggle[0].classList.add('light');
+        innerToggle[0].classList.add('light');
+        navCheckboxElement[0].classList.add('light');
+        navExtend[0].classList.add('light');
+        navContainer[0].classList.add('light');
+        darkModeToggle[0].classList.add('light');
+        navTheme[0].classList.add('light');
+        //Switch to a foreach
+        for(let i = 0; i < navBox.length; i++) {
+            navBox[i].classList.add('light');
+        }
+
+
+
+    } else {
+        outerToggle[0].classList.remove('light');
+        innerToggle[0].classList.remove('light');
+        navCheckboxElement[0].classList.remove('light');
+        navExtend[0].classList.remove('light');
+        navContainer[0].classList.remove('light');
+        darkModeToggle[0].classList.remove('light');
+        navTheme[0].classList.remove('light');
+        for(let i = 0; i < navBox.length; i++) {
+            navBox[i].classList.remove('light');
+        }
+
+
+    }
+})
